be08ae22aed34059bac168c1f1e123f0203c4595
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-08 23:59:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
     @word = word
   end
   
-  def match(array_of_words)
-     array_of_words.select do |element|
+  def match(some_of_words)
+     some_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end
   end
